4fab0911c6d5de2bcf9d394309470ad6fdce0678
Somnath Hazra <somnath6376hazra@gmail.com>
2022-11-12 10:50:15 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import BasicForm2 from './BasicForm2'
+import BasicForm from './BasicForm'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BasicForm/>
+      {/* <BasicForm2/> */}
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/BasicForm.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import './style.css'
+const BasicForm = () => {
+    
+    const [input,setInput] = useState({
+        email : "",
+        password : "",
+        id:""
+    });
+
+    const [allInput,setAllinput] = useState([]);
+    const [inputError,setInputError] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e) =>{
+        setInput(
+            {
+                ...input, id:new Date().getTime().toString(),[e.target.name] : e.target.value
+            }
+        )
+    }
+
+    const submitForm = (e) =>{
+        e.preventDefault();
+        setInputError(valid(input));
+        setIsSubmit(true);
+        console.log(inputError);
+    }
+
+    useEffect(() =>{
+        if(Object.keys(inputError).length === 0 && isSubmit){
+            setAllinput({...allInput,input})
+            console.log(allInput);
+            setInput(
+                {
+                email : "",
+                password : "",
+                id:""
+                }
+            )
+        }
+    },[inputError])
+
+    function valid(value){
+
+        let errors = {};
+        const regux = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if(!value.email){
+            errors.email = "Email is required!";
+        }else if(!regux.test(value.email)){
+            errors.email = "This is not valid Email!";
+        }
+        if(!value.password){
+            errors.password = "password is required!";
+        }else if(value.password.length < 4){
+            errors.password = "password will be more than 4 digit!";
+        }else if(value.password.length > 10){
+            errors.password = "password will be less than 10 digit!";
+        }
+        return errors;
+    }
+  return (
+    <>
+       {
+        Object.keys(inputError).length === 0 && isSubmit ? (<div className='SUCCESSFULLY'>SUCCESSFULLY SUBMITTED!</div>) : ( <pre>
+            {JSON.stringify(input,undefined,1)}
+        </pre>)
+       }
+        <form action="" onSubmit={submitForm}>
+        <h1>LOGIN</h1>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" name='email' id='email' placeholder='Enter Your Email' value={input.email} onChange={handleChange}/>
+                <p>{inputError.email}</p>
+            </div>
+           
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" name='password' id='password' placeholder='Enter Your Password' value={input.password} onChange={handleChange}/>
+                <p>{inputError.password}</p>
+            </div>
+          
+            <button type='submit'>Login</button>
+        </form>
+
+        {/* <div>
+            {
+                allInput.map((elm) =>{
+                    const {id,email,password} = elm;
+                    console.log(elm);
+                        return(
+                            <div key={id}>
+                                <p>{email}</p>
+                                <p>{password}</p>
+                            </div>
+                        )
+                })
+            }
+        </div> */}
+       
+    </>
+  )
+}
+
+export default BasicForm
+
+
